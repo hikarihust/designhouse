b07aa7103ba47ae6a2086dd536aa74b4c8a8e6df
vudinhquang <vudinhquangk53@gmail.com>
2020-10-07 14:01:54 +0900

    Resizing uploaded images

--- designhouse-api/app/Jobs/UploadImage.php
+++ designhouse-api/app/Jobs/UploadImage.php
@@ -7,19 +7,24 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Design;
+use Image;
+// use Illuminate\Support\Facades\Log;
 
 class UploadImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $design;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Design $design)
     {
-        //
+        $this->design = $design;
     }
 
     /**
@@ -29,6 +34,29 @@ class UploadImage implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $disk = $this->design->disk;
+        $filename = $this->design->image;
+        $original_file = storage_path() . '/uploads/original/'. $filename;
+
+        try{
+            // create the Large Image and save to tmp disk
+            Image::make($original_file)
+                ->fit(800, 600, function($constraint){
+                    $constraint->aspectRatio();
+                })
+                ->save($large = storage_path('uploads/large/'. $filename));
+
+
+            // Create the thumbnail image
+            Image::make($original_file)
+                ->fit(250, 200, function($constraint){
+                    $constraint->aspectRatio();
+                })
+                ->save($thumbnail = storage_path('uploads/thumbnail/'. $filename));
+
+            // store images to permanent disk
+        } catch(\Exception $e){
+            \Log::error($e->getMessage());
+        }
     }
 }
