a330067d3524fb315f11c7f44826ed5705ff795e
vudinhquang <vudinhquangk53@gmail.com>
2020-10-08 00:50:30 +0900

    Adding Tags to Designs

--- designhouse-api/app/Models/Design.php
+++ designhouse-api/app/Models/Design.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Cviebrock\EloquentTaggable\Taggable;
 
 class Design extends Model
 {
-    use HasFactory;
+    use HasFactory, Taggable;
 
     protected $fillable=[
         'user_id',
