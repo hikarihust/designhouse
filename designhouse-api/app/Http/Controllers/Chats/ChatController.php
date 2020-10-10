fd095a09f31da86bffda427affddaeb8681ee751
vudinhquang <vudinhquangk53@gmail.com>
2020-10-11 00:02:43 +0900

    Create the Chat Controller and Repositories

---
+++ designhouse-api/app/Http/Controllers/Chats/ChatController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Chats;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    // Send message to user
+    public function sendMessage(Request $request)
+    {
+
+    }
+
+    // Get chats for user
+    public function getUserChats()
+    {
+
+    }
+
+    // get messages for chat
+    public function getChatMessages($id)
+    {
+
+    }
+
+    // mark chat as read
+    public function markAsRead($id)
+    {
+
+    }
+
+    // destroy message
+    public function destroyMessage($id)
+    {
+
+    }
+}
---
+++ designhouse-api/app/Repositories/Eloquent/ChatRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Models\Chat;
+use App\Repositories\Contracts\IChat;
+
+class ChatRepository extends BaseRepository implements IChat
+{
+
+    public function model()
+    {
+        return Chat::class;
+    }
+}
