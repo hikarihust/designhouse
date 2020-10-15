27d0ab43149218d733bd5ea521d2a9857d39a310
vudinhquang <vudinhquangk53@gmail.com>
2020-10-15 14:15:53 +0900

    Setting up the Upload pages and components

--- designhouse-client/router.js
+++ designhouse-client/router.js
@@ -37,6 +37,16 @@ export function createRouter() {
         name: 'user.dashboard',
         component: page('user/dashboard.vue')
       },
+      {
+        path: '/upload',
+        name: 'designs.upload',
+        component: page('user/designs/create.vue')
+      },
+      {
+        path: '/designs/:id/edit',
+        name: 'designs.edit',
+        component: page('user/designs/edit.vue')
+      },
     ]
   })
 }
